508bf686ca448ed9ecfe290d73d120aa208def05
ali-haider-381 <143490110+ali-haider-381@users.noreply.github.com>
2023-10-28 23:41:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,94 @@
+import inquirer from "inquirer";
+let account = {
+    accountNumber: "123456789",
+    pin: "2222",
+    balance: 10000,
+};
+async function login() {
+    const input = await inquirer.prompt([
+        {
+            name: "accountNumber",
+            type: "string",
+            message: "Enter your account number"
+        },
+        {
+            name: "pin",
+            type: "password",
+            message: "Please enter your pin",
+            mask: "*"
+        }
+    ]);
+    if (input.accountNumber === account.accountNumber && input.pin === account.pin) {
+        console.log("Your login is successful");
+        return true;
+    }
+    else {
+        console.log("Login error");
+        return false;
+    }
+}
+;
+function checkBalance() {
+    console.log(`Your account balance is ${account.balance}`);
+}
+async function withdrawAmount() {
+    const { amount } = await inquirer.prompt([
+        {
+            name: "amount",
+            type: "number",
+            message: "Enter amount you want to withdraw"
+        }
+    ]);
+    if (amount <= account.balance) {
+        account.balance -= amount;
+        ({
+            type: 'Withdrawal',
+            amount,
+            date: new Date()
+        });
+        console.log("Your withdrawal was successful");
+    }
+    else {
+        console.log("Insufficient balance");
+    }
+}
+// function showTransactions() {
+// //   for (let i = 0; i < account.transactions.length; i++)
+//    {
+//     // const transaction = account.transactions[i];
+//     // console.log(transaction);
+//   }
+// }
+async function performAction() {
+    const { action } = await inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Check Balance", "Withdraw", "View Transactions", "Logout"],
+        },
+    ]);
+    switch (action) {
+        case "Check Balance":
+            checkBalance();
+            break;
+        case "Withdraw":
+            await withdrawAmount();
+            break;
+        case "View Transactions":
+            //   showTransactions();
+            break;
+        case "Logout":
+            console.log("You have logged out successfully");
+            break;
+        default:
+            console.log("Invalid action");
+    }
+}
+async function main() {
+    const loggedIn = await login();
+    if (loggedIn) {
+        await performAction();
+    }
+}
+main();
